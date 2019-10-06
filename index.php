8b5197de7b90839fb25804b815c6aa586fcd1f85
AishaMills <56207670+AishaMills@users.noreply.github.com>
2019-10-05 23:13:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
-     
-    <?php 
-
-        $title = "Index";
-    
+ 
+   <?php 
+        $title = "index";
         include 'includes/header.php' 
-    
     ?>
-    
-
-   
-  
     <!-- Basic HTML -->
     <h1>Hello HTML - PHP Primer</h1>
     <br/>
     <?php  
         //Printing to HTML using echo
         echo 'Hello PHP!';
-        // You can echo HTML tags
+        // YOu can echo HTML tags
         echo '<br/>';
         echo 'Second Line';
         echo '<br/>';
@@ -32,9 +25,10 @@
         echo "<h1>My Name is: $name </h1>";
     ?>
     <button type="button" class="btn btn-dark">CLICK ME!</button>
-   
-    <!--<a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a> -->
+    <button type="button" class="btn btn-primary">CLICK ME!</button>
+    <button type="button" class="btn btn-success">CLICK ME!</button>
+    <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a>
     <?php
         require 'includes/footer.php' 
-    ?> 
+    ?>
     
